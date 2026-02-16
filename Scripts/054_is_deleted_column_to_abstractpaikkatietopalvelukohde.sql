2d32af9233ba89be57679ba6914dc9bd16729c55
Tuomas Kujala <tuomas.kujala@sitowise.com>
2026-02-16 12:49:48 +0200

    fix: add column also for liite table and set columns as not null

--- Scripts/054_is_deleted_column_to_abstractpaikkatietopalvelukohde.sql
+++ Scripts/054_is_deleted_column_to_abstractpaikkatietopalvelukohde.sql
@@ -1,2 +1,5 @@
 ALTER TABLE kohteet.abstractpaikkatietopalvelukohde
-    ADD COLUMN is_deleted boolean DEFAULT false;
+    ADD COLUMN is_deleted boolean DEFAULT false NOT NULL;
+
+ALTER TABLE kohteet.liite
+    ADD COLUMN is_deleted boolean DEFAULT false NOT NULL;
